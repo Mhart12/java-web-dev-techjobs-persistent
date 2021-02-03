f9a5cc574dd7f058d92a7c530e4abaf743e6cf62
Monica Hart <monica.hart@vmlyr.com>
2021-02-03 17:52:28 -0600

    Finished P1, working through P2 up until Models section

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,14 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import java.util.Objects;
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Field is required.")
+    @Size(max = 100, message = "Brah this field can't take any more characters. Better think of cutting this down a smidge.")
     private String name;
 
+    /**
+     * Each (subclass)(child)(or any class extending AbstractEntity) will inherit the 'name' field*/
     public int getId() {
         return id;
     }
