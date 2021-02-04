7485719658884a3795663a1d295a066a45e63a55
Monica Hart <monica.hart@vmlyr.com>
2021-02-03 18:07:06 -0600

    P2, completed model section

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+
+    @NotBlank(message = "Field is required.")
+    @Size(max = 50, message = "Brah this field can't take any more characters. Better think of cutting this down a smidge.")
+    private String location;
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank(message = "Field is required.")
+    @Size(min=15, max=300)
+    private String description;
+
+    public Skill() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
